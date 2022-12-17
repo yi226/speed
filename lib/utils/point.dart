26aaa954d2b7eb27f1e27ae35825c70f4d83f662
yi226 <63433206+yi226@users.noreply.github.com>
2022-12-17 20:30:48 +0800

    add doc

--- lib/utils/point.dart
+++ lib/utils/point.dart
@@ -3,7 +3,14 @@ import 'package:fluent_ui/fluent_ui.dart';
 class Point {
   double x;
   double y;
+  double a;
+  double w;
   Offset control;
 
-  Point({this.x = 0, this.y = 0, this.control = const Offset(0, 0)});
+  Point(
+      {this.x = 0,
+      this.y = 0,
+      this.a = 0,
+      this.w = 0,
+      this.control = const Offset(0, 0)});
 }
